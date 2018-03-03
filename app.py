1776ec3553ee6f02dfbe0b09de65a91298b701f8
Mohamed Ashmawy <ashma010@umn.edu>
2018-03-03 18:27:51 -0500

    justlinks: POST and GET for links
    - POST requires title and url for a link to get added

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template
+from flask_restful import Resource, Api, abort, reqparse
+
+app = Flask(__name__)
+api = Api(app)
+
+links = {'active': []}
+
+parser = reqparse.RequestParser()
+parser.add_argument('title')
+parser.add_argument('url')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+class all_links(Resource):
+	def get(self):
+		return links
+	
+	def post(self):
+		args = parser.parse_args()
+		if not args['url']:
+			return 'Missing url argument!'
+		if not args['title']:
+			return 'Missing title argument!'
+
+		links['active'].append({'title': args['title'], 'url': args['url']})
+
+		return links['active'], 201
+
+api.add_resource(all_links,'/all_links')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
