34630670fb6abd325304849993fee8045fc88ba0
zubair <zubair@beeswax.com>
2018-03-04 20:04:30 -0500

    added endpoint: all_links_by_status

--- app.py
+++ app.py
@@ -4,11 +4,12 @@ from flask_restful import Resource, Api, abort, reqparse
 app = Flask(__name__)
 api = Api(app)
 
-links = {'active': []}
+links = {'active': [], 'inactive': []}
 
 parser = reqparse.RequestParser()
 parser.add_argument('title')
 parser.add_argument('url')
+parser.add_argument('status')
 
 @app.route('/')
 def home():
@@ -29,7 +30,17 @@ class all_links(Resource):
 
 		return links['active'], 201
 
+class all_links_by_status(Resource):
+    def get(self):
+        args = parser.parse_args()
+        if not args['status']:
+            return 'Missing status argument!'
+        if args['status'] not in links:
+            return 'Invalid status argument!'
+        return links[args['status']]
+
 api.add_resource(all_links,'/all_links')
+api.add_resource(all_links_by_status,'/all_links_by_status')
 
 
 if __name__ == '__main__':
